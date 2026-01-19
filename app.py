50f0c01297f430f694e8dbcb4cedcf12fce4637a
Jason <151385012+HakaseArt@users.noreply.github.com>
2026-01-19 14:40:50 -0800

    Add files via upload
    Improved code and refactored HTML code

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 import os
+import json
+import mimetypes
 
-base_dir = os.path.dirname("Python/week3_server/")
-print(base_dir)
+# Sets a var for path of file root to current dir, __file__ points to where the script is
+root_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Handler class to handle HTTP requests
 class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if os.path.exists(base_dir):
-            if self.path == "/":
-                print("path")
-                filename = "index.html"
-            else:
-                filename = self.path
-        else:
-            self.send_error(404, f"File Not Found: {base_dir}")
-        
-        try:
 
+    #GET method for getting data from client
+    def do_GET(self):
+
+        if self.path == "/":
+            filename = os.path.join(root_dir, "index.html")
+        else:
+            filename = self.path.lstrip("/")
+
+        # Will automatically check file type
+        content_type = mimetypes.guess_type(filename)
+        # Default case if no type was 
+        if not content_type:
+            content_type = "application/octet-stream"
+
+        print(filename)
+        try:
             with open(filename, "r", encoding="utf-8") as f:
                 print("Writing File") 
                 html_content = f.read()
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", content_type)
             self.end_headers()
 
             self.wfile.write(bytes(html_content, "utf-8"))
@@ -30,6 +38,7 @@ class Handler(BaseHTTPRequestHandler):
         except FileNotFoundError:
             self.send_error(404, f"File Not Found: {self.path}")
 
+    #POST method for parsing data from user input and returning with new HTML
     def do_POST(self):
         try:
             content_length = int(self.headers.get("Content-Length", 0))
@@ -47,7 +56,11 @@ class Handler(BaseHTTPRequestHandler):
             pass
 
 
+#Sets server address and port
 server_address = ("localhost", 8000)
+#Creates server, using the IP address and sets handler
 httpd = HTTPServer(server_address, Handler)
+#Debug
 print("Running on localhost:8000")
+#Runs server indefinitely
 httpd.serve_forever()
